61477df7c0ce092d56dc9705690c78fbc318be92
Alessio <alessioferriweb@gmail.com>
2020-04-04 19:52:00 +0100

    Making comment component configurable via props

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author={faker.name.firstName()} />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45 PM"
+        content="Nice thing!"
+        avatarUrl={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 2:32 PM"
+        content="Cool comment!"
+        avatarUrl={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 1:17 PM"
+        content="I like React!"
+        avatarUrl={faker.image.avatar()}
+      />
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
