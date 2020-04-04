c1765f0ab83a5bfa814b9f42c96070eac45e96df
Alessio <alessioferriweb@gmail.com>
2020-04-04 18:40:55 +0100

    Adding form and css properties as JS objects

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
