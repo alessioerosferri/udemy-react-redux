ac7b51f5521e0607d11ea5d3521c3a89ec7c76fc
Alessio <alessioferriweb@gmail.com>
2020-04-05 18:43:13 +0100

    Adding some tests

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
 const seasonConfig = {
   summer: {
@@ -22,10 +23,10 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const {text, iconName} = seasonConfig[season];
   return (
-    <div>
-      <i className={`massive ${iconName} icon`}> </i>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}> </i>
       <h1>{text}</h1>
-      <i className={`massive ${iconName} icon`}> </i>
+      <i className={`icon-right massive ${iconName} icon`}> </i>
     </div>
   )
 };
