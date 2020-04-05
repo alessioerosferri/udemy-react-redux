267a20d995a8404239fd18bebb09ab4af117b208
Alessio <alessioferriweb@gmail.com>
2020-04-05 16:05:25 +0100

    Wrapping the comments in the ApprovalCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 4:45 PM"
-        content="Nice thing!"
-        avatarUrl={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 2:32 PM"
-        content="Cool comment!"
-        avatarUrl={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 1:17 PM"
-        content="I like React!"
-        avatarUrl={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45 PM"
+          content="Nice thing!"
+          avatarUrl={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:32 PM"
+          content="Cool comment!"
+          avatarUrl={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 1:17 PM"
+          content="I like React!"
+          avatarUrl={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
