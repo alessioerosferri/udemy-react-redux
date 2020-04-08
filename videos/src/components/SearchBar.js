e7d84ce5ce7a76f879ec745bfe79676b905a22a8
Alessio <alessioferriweb@gmail.com>
2020-04-08 19:15:05 +0100

    On submit send to App the term

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from "react";
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
+  state = {term: ""};
+  onInputChange = (event) => {
+    this.setState({term: event.target.value});
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
-    return <div>SearchBar</div>
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
