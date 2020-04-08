23943c505c6b5600e66852af5e79f752c3ed4534
Alessio <alessioferriweb@gmail.com>
2020-04-08 19:42:48 +0100

    Adding video list component rendering

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = "AIzaSyDwV5Sc4VhfXQa2qbN3tigj0KmLU-mVwPw";
 
@@ -24,7 +25,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onTermSubmit}/>
-        I have {this.state.videos.length} videos
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
