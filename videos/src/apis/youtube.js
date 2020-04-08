b7c38f906a13c355876ecf4732f90234a592e043
Alessio <alessioferriweb@gmail.com>
2020-04-08 19:12:48 +0100

    Init api youtube file

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+const KEY = "AIzaSyDwV5Sc4VhfXQa2qbN3tigj0KmLU-mVwPw";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+})
