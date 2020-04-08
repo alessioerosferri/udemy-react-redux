35c3fcb0e1ed85df8a04e765bc4272fa43977cac
Alessio <alessioferriweb@gmail.com>
2020-04-08 19:20:12 +0100

    Fixing bug with axios that is not merging the params. Workaround

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,13 +1,5 @@
 const axios = require("axios");
 
-const KEY = "AIzaSyDwV5Sc4VhfXQa2qbN3tigj0KmLU-mVwPw";
-
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    type: "video",
-    key: KEY
-  }
+  baseURL: 'https://www.googleapis.com/youtube/v3'
 })
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-class App extends React.Component{
+import youtube from "../apis/youtube";
+
+const KEY = "AIzaSyDwV5Sc4VhfXQa2qbN3tigj0KmLU-mVwPw";
+
+
+class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get("/search", {
+      params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+        q: term
+      }
+    })
+  }
+
   render() {
     return (
-      <div>
-        <SearchBar/>
+      <div className="ui container">
+        <SearchBar onSubmit={this.onTermSubmit}/>
       </div>
     )
   }
