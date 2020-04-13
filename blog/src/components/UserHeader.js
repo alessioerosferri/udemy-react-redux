48ef1c0ebd875abdd518ea06796e9573e3f37b26
Alessio <alessioferriweb@gmail.com>
2020-04-13 20:28:23 +0100

    Adding author's name for each post

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,11 +1,26 @@
 import React from "react";
+import {connect} from "react-redux";
+import {fetchUser} from "../actions";
 
 class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
+    const user = this.props.users.find((user) => {
+      return user.id === this.props.userId;
+    });
+    if (!user) {
+      return null;
+    }
     return (
-      <div>User header</div>
+      <div className="header">{user.name}</div>
     );
   }
 }
 
-export default UserHeader;
+const mapStateToProps = ({users}) => {
+  return {users};
+}
+export default connect(mapStateToProps, {fetchUser})(UserHeader);
