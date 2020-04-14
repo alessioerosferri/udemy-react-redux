429723130f86d758c0ce227afa0ca4ce0fd34ae8
Alessio <alessioferriweb@gmail.com>
2020-04-14 18:13:29 +0100

    Using link component for navigation

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Link} from "react-router-dom";
 
 const PageOne = ()=>{
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo"> page two </Link>
+    </div>
+  );
 }
 
 const PageTwo = ()=>{
-  return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo
+      <Link to="/"> page one </Link>
+    </div>
+  );
 }
 
 const App = ()=>{
