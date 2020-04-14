36a3df251ffa5da8a67ed36ccc4f2dba457ef6c1
Alessio <alessioferriweb@gmail.com>
2020-04-14 15:51:28 +0100

    Refactoring

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {connect} from "react-redux";
-import {fetchUser} from "../actions";
 
 class UserHeader extends React.Component {
 
@@ -22,4 +21,4 @@ const mapStateToProps = ({users}, ownProps) => {
     })
   }
 }
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
