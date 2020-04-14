bf576a716447bccf25d5f1445f71c75548dfe7c8
Alessio <alessioferriweb@gmail.com>
2020-04-14 18:34:54 +0100

    Init routes to streams components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,31 +1,21 @@
 import React from "react";
-import {Route, Link, BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
-const PageOne = ()=>{
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo"> page two </Link>
-    </div>
-  );
-}
-
-const PageTwo = ()=>{
-  return (
-    <div>
-      PageTwo
-      <Link to="/"> page one </Link>
-    </div>
-  );
-}
-
-const App = ()=>{
+const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new" exact component={StreamCreate}/>
+          <Route path="/streams/edit" exact component={StreamEdit}/>
+          <Route path="/streams/delete" exact component={StreamDelete}/>
+          <Route path="/streams/show" exact component={StreamShow}/>
         </div>
       </BrowserRouter>
     </div>
