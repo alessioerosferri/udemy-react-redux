b575b8ccdc3f3440edab9585251161028df725de
Alessio <alessioferriweb@gmail.com>
2020-04-15 13:58:01 +0100

    Init library gapi to use oauth

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: "1066937536807-aoug58sa05n99a25iard9gdcvssq1e0u.apps.googleusercontent.com",
+        scope: "email"
+      })
+    });
+  }
+
   render() {
     return (
       <div>
