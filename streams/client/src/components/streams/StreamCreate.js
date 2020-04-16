f94e413aeebc1c097037fba6e36916c96411b0d2
Alessio <alessioferriweb@gmail.com>
2020-04-16 19:03:18 +0100

    removing unused line

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -9,7 +9,6 @@ class StreamCreate extends React.Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <form>
         <Field name="title" component={this.renderInput}/>
