d8aecc6c83e702f3d7a41cdcf4056c07f04869fa
Alessio <alessioferriweb@gmail.com>
2020-04-18 17:34:15 +0100

    Completing reducer

--- streams/client/src/reducers/streamsReducer.js
+++ streams/client/src/reducers/streamsReducer.js
@@ -1,9 +1,16 @@
-import {EDIT_STREAM} from "../actions/types";
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM} from "../actions/types";
+import _ from "lodash";
 
 export default (state = {}, {type, payload}) => {
   switch (type) {
     case EDIT_STREAM:
+    case FETCH_STREAM:
+    case CREATE_STREAM:
       return {...state, [payload.id]: payload};
+    case DELETE_STREAM:
+      return _.omit(state, action.payload)
+    case FETCH_STREAMS:
+      return {...state, ..._.mapKeyes(payload, "id")};
     default:
       return state;
   }
