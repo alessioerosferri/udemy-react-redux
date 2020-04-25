d107d41bc3f2b5f877890b9876f08c062d2c451e
Alessio <alessioferriweb@gmail.com>
2020-04-25 18:44:23 +0100

    Adding forms

--- context-example/src/components/App.js
+++ context-example/src/components/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component {
+  state = {language: "english"};
+
+  onLanguageChange = language => {
+    this.setState({language})
+  };
+
   render() {
     return (
-      <div>App</div>
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag uk" onClick={() => {
+            this.onLanguageChange("english")
+          }}/>
+          <i className="flag nl" onClick={() => {
+            this.onLanguageChange("dutch")
+          }}/>
+        </div>
+        <UserCreate/>
+      </div>
     );
   }
 }
