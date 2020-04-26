d97e716d0f7907f9c50b4fd06d1f941b58bbc0b4
Alessio <alessioferriweb@gmail.com>
2020-04-26 18:03:45 +0100

    App passing value to context using the Provider

--- context-example/src/components/Button.js
+++ context-example/src/components/Button.js
@@ -5,7 +5,7 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === "engligh" ? "Submit" : "Voorleggen";
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
     return (
       <button className="ui button primary">{text}</button>
     );
