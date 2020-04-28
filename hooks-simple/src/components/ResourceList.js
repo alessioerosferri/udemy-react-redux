1c2a805ed362171631eed599dc2a8fe3cad4c75b
Alessio <alessioferriweb@gmail.com>
2020-04-28 13:35:15 +0100

    Using useEffect with dependencies. Everytime the dependency gets updated it will call the function passed again

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const ResourceList = () => {
+const ResourceList = ({resource}) => {
   const [resources, setResources] = useState([]);
   const fetchResource = async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
     setResources(response.data);
   };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
 
   return (
     <div>
